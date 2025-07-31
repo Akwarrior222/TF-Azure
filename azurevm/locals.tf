fb5d80fef628c29e1bcd27a8d3360b06974bd324
shar1810 <69445643+shar1810@users.noreply.github.com>
2025-07-31 11:17:14 +0530

    Update locals.tf

--- azurevm/locals.tf
+++ azurevm/locals.tf
@@ -1,4 +1,4 @@
-locals {
+/* locals {
   default_startup_script = templatefile("${path.module}/templates/userdata.sh", {
     ssh_port = var.ssh_port
   })
@@ -6,4 +6,4 @@ locals {
   startup_script = join("\n\n", [local.default_startup_script, var.custom_startup_script])
 
   instance_name = "${var.project_prefix}-${var.instance_name}"
-}
+} */
